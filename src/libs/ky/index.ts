97f1e013dde7431b2e5a6065154701102e6356d4
Andrey Filippov <androsphilippos@gmail.com>
2023-11-08 17:35:30 +0500

    fix: setting vite.config

--- src/libs/ky/index.ts
+++ src/libs/ky/index.ts
@@ -1,7 +1,7 @@
 import ky from 'ky';
 
 export const kyInstance = ky.create({
-  prefixUrl: import.meta.env.BASE_URL,
+  prefixUrl: import.meta.env.VITE_BASE_URL,
   headers: { Accept: 'application/json', 'Content-Type': 'application/x-www-form-urlencoded' },
   timeout: 60000,
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-})
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [svelte(), basicSsl()],
+    server: {
+      host: env.VITE_HOST,
+      https: true,
+      port: 3000,
+    },
+  }
+});
